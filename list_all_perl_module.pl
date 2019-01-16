83685b42648006d31feb25ad9ed5f5e0b5f4bc5a
krajuskar <krajuskar@gmail.com>
2019-01-16 15:12:45 +0530

    Update list_all_perl_module.pl

--- list_all_perl_module.pl
+++ list_all_perl_module.pl
@@ -7,3 +7,16 @@ print "Current List of Installed PERL Modules:\n\n";
 foreach my $mod(@Modules){
 	print "$mod\n";
 }
+__END__
+we can also use some basic commands
+perldoc perlmodlib
+perldoc perllocal
+perldoc Regexp::Common
+perldoc -m Regexp::Common
+
+specific perl function, use the option -f
+perldoc -f splice
+
+The perl array @INC contains the list of places that the ‘do EXPR’, ‘require’, or ‘use’ constructs look for their library files. The following one-liner shows the contents of the @INC perl array:
+perl -e 'foreach $folder (@INC) { print "$folder\n";}'
+
